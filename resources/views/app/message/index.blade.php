8cf34e9cae85f2b18d53ada97e0a43eb72ade811
Dr.XGB <dr.xgb.rm2k@gmail.com>
2021-11-15 21:22:03 -0300

    SASS Implementation

--- resources/views/app/message/index.blade.php
+++ resources/views/app/message/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('index')
 
 @section('content')
 	<h1>Mensagens</h1>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,14 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="css/app.css" />
+    <title>{{ env('APP_NAME') }}</title>
+</head>
+<body>
+    @section('content')
+    @show
+</body>
+</html>
